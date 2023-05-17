e2b22bc3514b1bbf3edd36b8dc440b67effdf86e
Artem Bubnov <artem.bubnov@vrpconsulting.com>
2023-05-17 16:52:20 +0300

    test_12

--- build/install_dependencies.sh
+++ build/install_dependencies.sh
@@ -1,12 +1,13 @@
 echo -e "--- SFDX CLI installation script executions start ---\n\n\n"
 
 
-echo -e"\n--- Step 1. Installing Salesforce CLI ---"
+echo "--- Step 1. Installing Salesforce CLI execution ---"
 sudo npm install -global sfdx-cli
 echo -e "--- Step 1 execution is finished ---\n"
 
 
-echo -e "--- Step 2. Salesforce CLI version check ---\n"
+echo -e "--- Step 2. Salesforce CLI version check ---"
 sudo npm install sfdx --version
+echo -e "--- Step 2 execution is finished ---"
 
 echo -e "\n\n\n--- SFDX CLI installation script executions end ---"
--- build/login_to_SF_org.sh
+++ build/login_to_SF_org.sh
@@ -10,11 +10,60 @@ echo $TARGET_BRANCH_NAME
 
 
 
+echo -e "\n\n\nStep 2. Define case for the current pipeline"
+echo "Depends on the result of case definition the following will be determined:"
+echo "A - Salesforce access key"
+echo "B - Salesforce org alias"
 
-echo "login test"
+case $TARGET_BRANCH_NAME in
+    "dev")
+        CASE_LOG="qa"
+        #ACCESS_KEY_SF=$ACCESS_KEY_SF_DELTAQA_SANDBOX
+        SALESFORCE_ORG_ALIAS="salesforce_qa_sandbox.org"
+        ;;
+    "staging")
+        CASE_LOG="staging"
+        #ACCESS_KEY_SF=$ACCESS_KEY_SF_STAGING
+        SALESFORCE_ORG_ALIAS="salesforce_staging.org"
+        ;;
+    "uat")
+        CASE_LOG="uat"
+        #ACCESS_KEY_SF=$ACCESS_KEY_SF_UAT
+        SALESFORCE_ORG_ALIAS="salesforce_uat.org"        
+        ;;
+    "prod")
+        CASE_LOG="prod"
+        #ACCESS_KEY_SF=$ACCESS_KEY_SF_PROD
+        SALESFORCE_ORG_ALIAS="salesforce_prod.org"
+        ;;
+    *)
+        echo "Not valid"
+        ;;
+esac
 
-#echo "TEST"
-#echo $GITHUB_REF_NAME
+echo "Step 2 execution is finished"
+echo "Step 2 execution result:"
+echo "Case result:"
+echo $CASE_LOG
+echo "Salesforce org to be used:"
+echo $CASE_LOG
+echo "Salesforce alias to be used: "
+echo $SALESFORCE_ORG_ALIAS
+
+
+
+
+echo -e "\n\n\nStep 3. Login to the target Salesforce org"
+echo "Creating .key file"
+#touch access_pass.key
+
+echo -e "\nAdding access data to .key file"
+#echo $ACCESS_KEY_SF > access_pass.key
+
+echo -e "\nTry SF login"
+#sfdx force:auth:sfdxurl:store -f "access_pass.key" -a ${SALESFORCE_ORG_ALIAS} -d
+#rm access_pass.key
+echo "Step 3 execution is finished"
 
 
 
