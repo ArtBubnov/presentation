5ca3d42aba0bc984bbd01eb9cb746fa91e48bd21
Artem Bubnov <artem.bubnov@vrpconsulting.com>
2023-05-17 15:49:29 +0300

    test_04

--- build/test.sh
+++ build/test.sh
@@ -1,7 +1,10 @@
 #Installs the sfdx cli
+echo -e "--- Script executions start ---\n\n\n"
 
 echo "Installing Salesforce CLI"
 sudo npm install -global sfdx-cli
 
-echo -e "\n\n\nSalesforce CLI version check\n\n\n"
+echo -e "\n\n\nSalesforce CLI version check"
 sudo npm install sfdx --version
+
+echo -e "\n\n\n--- Script executions end ---"
