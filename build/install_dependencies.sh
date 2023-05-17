4bd79d650d81f4aa7b4d7277fdcfca93649f8096
Artem Bubnov <artem.bubnov@vrpconsulting.com>
2023-05-17 16:37:23 +0300

    test_07

--- build/install_dependencies.sh
+++ build/install_dependencies.sh
@@ -1,10 +1,13 @@
 #Installs the sfdx cli
 echo -e "--- SFDX CLI installation script executions start ---\n\n\n"
 
-echo "Installing Salesforce CLI"
-sudo npm install -global sfdx-cli
 
-echo -e "\n\n\nSalesforce CLI version check"
+echo -e"\n--- Step 1. Installing Salesforce CLI ---"
+sudo npm install -global sfdx-cli
+echo -e "--- Step 1 execution is finished ---\n"
+
+
+echo -e "--- Step 2. Salesforce CLI version check ---\n"
 sudo npm install sfdx --version
 
 echo -e "\n\n\n--- SFDX CLI installation script executions end ---"
