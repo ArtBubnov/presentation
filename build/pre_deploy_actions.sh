71e9bf4198893a674f63a59c149129e5ea4ed7ee
Artem Bubnov <artem.bubnov@vrpconsulting.com>
2023-05-18 17:40:27 +0300

    test_36

--- build/pre_deploy_actions.sh
+++ build/pre_deploy_actions.sh
@@ -70,7 +70,7 @@ echo $(git config pull.rebase false)
 echo $(git config advice.detachedHead false)
 
 #git checkout "origin/"$SOURCE_BRANCH_NAME
-git checkout $SOURCE_BRANCH_NAME
+#git checkout $SOURCE_BRANCH_NAME
 git pull origin $TARGET_BRANCH_NAME --no-commit && git commit -m "Merge" || true
 
 echo -e "\n--- Step 2.1 execution is finished ---"
