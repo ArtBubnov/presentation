3bc494bd351e727d1a042b0bbbb5b739e4d315d4
Artem Bubnov <artem.bubnov@vrpconsulting.com>
2023-05-18 16:35:34 +0300

    test_21

--- build/pre_deploy_actions.sh
+++ build/pre_deploy_actions.sh
@@ -5,13 +5,15 @@ echo -e "--- Predeploy actions script executions start ---\n\n\n"
 
 
 echo -e "--- Step 1. Define global variables for the current pipeline ---\n"
+
 SOURCE_BRANCH_NAME=$GITHUB_HEAD_REF
 TARGET_BRANCH_NAME=$GITHUB_BASE_REF
-echo "Global variables display:"
+echo -e "Global variables display:\n"
 echo "Event type is:"
-echo "Pull request"
+echo -e "Pull request\n"
 echo "Source branch name is:"
 echo $SOURCE_BRANCH_NAME
-echo "Target branch name is:"
+echo -e "\nTarget branch name is:"
 echo $TARGET_BRANCH_NAME
+
 echo -e "\n--- Step 1 execution is finished ---"
